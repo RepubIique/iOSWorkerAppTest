214f2d10bc3f0798ad1f5f1568545cad403e6f1c
Kendrick Bong <Kendrickbong1996@gmail.com>
2021-10-09 16:16:28 +0800

    Initial Commit

---
+++ MyPassWorkerApp/MyPassWorkerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPassWorkerAppApp.swift
+//  MyPassWorkerApp
+//
+//  Created by Kendrick Bong on 9/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MyPassWorkerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyPassWorkerAppUITests/MyPassWorkerAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MyPassWorkerAppUITestsLaunchTests.swift
+//  MyPassWorkerAppUITests
+//
+//  Created by Kendrick Bong on 9/10/21.
+//
+
+import XCTest
+
+class MyPassWorkerAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
